79d19097f5f155b4c0b253e6bcc84a87321ec00f
Tom Pound <tompound@ymail.com>
2020-02-02 13:50:08 +0000

    Prototype Started

--- src/AppPrototype.java
+++ src/AppPrototype.java
@@ -1,9 +1,21 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class AppPrototype {
-
+public class AppPrototype extends Application{
+	private int defaultXSize = 640;
+	private int defaultYSize = 360;
+		
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Application Started...");
+		launch(args);
+		System.out.println("Application Finished...");
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//Start
+		
+		
 	}
 
 }
